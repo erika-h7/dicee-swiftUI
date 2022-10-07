a9846ff74d1ca08220ee439414f5cdfc10eb987e
erika-h7 <erika.herrera1@outlook.com>
2022-10-07 10:43:33 -0700

    Initial Commit

---
+++ Dicee/DiceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeApp.swift
+//  Dicee
+//
+//  Created by Infinity Code on 10/7/22.
+//
+
+import SwiftUI
+
+@main
+struct DiceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
